da8033e5bea437dad58d9c52fb1392f958263a6d
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2025-02-01 17:28:18 +0100

    Implement world wrapping

--- player.gd
+++ player.gd
@@ -3,12 +3,11 @@ extends RigidBody2D
 @export var spin_power = 8000
 var thrust = Vector2.ZERO
 var rotation_dir = 0
-
+var screensize := Vector2.ZERO
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
-
-
-
+var size := Vector2.ZERO
+var radius := 0.0
 func change_state(new_state):
 	match new_state:
 		INIT:
@@ -22,6 +21,12 @@ func change_state(new_state):
 	state = new_state
 
 func _ready() -> void:
+	screensize = get_viewport_rect().size
+	size = Vector2(
+		$Sprite2D.texture.get_width(),
+		$Sprite2D.texture.get_height()
+	) * $Sprite2D.scale
+	radius = size.x / 2.0
 	change_state(ALIVE)
 
 func _process(delta: float) -> void:
@@ -33,8 +38,15 @@ func get_input():
 		return
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
-		rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
 
 func _physics_process(delta: float) -> void:
 	constant_force = thrust	
 	constant_torque = rotation_dir * spin_power
+	
+func _integrate_forces(physics_state):
+	
+	var xform = physics_state.transform	
+	xform.origin.x = wrapf(xform.origin.x, -radius, screensize.x+radius)
+	xform.origin.y = wrapf(xform.origin.y, -radius, screensize.y+radius)
+	physics_state.transform = xform
