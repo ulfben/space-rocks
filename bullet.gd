a63e5c9fe3b3e9de35e6cdf8a17ba7b816437eeb
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2025-02-01 19:15:19 +0100

    rocks explode when shot

--- bullet.gd
+++ bullet.gd
@@ -9,10 +9,8 @@ func start(_transform):
 func _process(delta: float) -> void:
 	position += velocity * delta
 
-
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
-
 
 func _on_bullet_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("rocks")):
--- rock.gd
+++ rock.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+signal exploded
 var screensize = Vector2.ZERO
 var size = 0
 var radius = 0
@@ -12,6 +13,7 @@ func start(_position, _velocity, _size):
 	position = _position
 	size = _size
 	mass = 1.5 * size
+	$Explosion.scale = Vector2.ONE * 0.75 * size
 	$Sprite2D.scale = Vector2.ONE * scale_factor * size
 	radius = int($Sprite2D.texture.get_size().x / 2 * $Sprite2D.scale.x)
 	var shape = CircleShape2D.new()
@@ -29,3 +31,14 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0 - radius, screensize.x + radius)
 	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)
 	physics_state.transform = xform
+
+func explode():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
